365025d49b2ff1a06617744848b11c06a8efefef
vusis <47633975+vusis@users.noreply.github.com>
2021-09-05 09:46:55 +0200

    removing commented out code

--- main.js
+++ main.js
@@ -123,19 +123,4 @@ function remove_duplicates(arr) {
     }
     return ret_arr;
   }
-/** 
-  function WriteToFile(passForm) {
- 
-    set fso = CreateObject("Scripting.FileSystemObject"); 
-    set s   = fso.CreateTextFile("<your Path>/filename.txt", True);
- 
-    var firstName = document.getElementById('FirstName');
-    var lastName  = document.getElementById('lastName');
- 
-    s.writeline("First Name :" + FirstName);
-    s.writeline("Last Name :" + lastName);
- 
-    s.writeline("-----------------------------");
-    s.Close();
- }
- */
+
